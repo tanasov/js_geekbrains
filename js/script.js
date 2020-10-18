f7964b51747d576998877be1dbcc7bf7eb598aff
Aleksandr <aleksandr@MacBook-Air-Aleksandr.local>
2020-10-18 15:58:28 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+// 1 Задание
+let Tc = 4;
+let tf = (9 / 5) * Tc + 32;
+alert(tf);
+// 2 задание
+let Name = 'Василий';
+let admin;
+admin = Name;
+alert(admin);
+// 3 Задание
+let myFirst = 1000 + '108';
+console.log(myFirst);
+// Получится 1000108, т.к. произойдет сложение числа и строки
+// 4 Задание
+// Defer ждет весь документ html, async - нет
